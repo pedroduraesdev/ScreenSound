703590817a12abb60984313f6e74184574f724e2
Pedro Durães <pedroduraes77@gmail.com>
2024-07-31 12:58:52 -0300

    first commit

---
+++ Musica.cs
@@ -0,0 +1,19 @@
+class Musica {
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica() {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if(disponivel){
+            Console.WriteLine("Disponícel no plano");
+        } else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
+    }
+
+}
